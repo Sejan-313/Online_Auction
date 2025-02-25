8d1ab8c543e78592d03d4be84ea904dbe998a445
Sejan-313 <sezansahin3011@gmail.com>
2025-02-25 08:35:12 +0530

    chnages commited

--- Frontend/src/role/user/components/Latestlink.jsx
+++ Frontend/src/role/user/components/Latestlink.jsx
@@ -4,13 +4,13 @@ const Latestlink = () =>
     <div className="row">
         <div className="col-lg-12 text-center">
             <div className="section-title">
-                <h2>Latest Products</h2>
+                <h2>Products For Auction</h2>
             </div>
             <ul className="product-controls">
                 <li data-filter="*">All</li>
-                <li data-filter=".dresses">Dresses</li>
-                <li data-filter=".bags">Bags</li>
-                <li data-filter=".shoes">Shoes</li>
+                <li data-filter=".dresses">Electronic</li>
+                <li data-filter=".bags">Furniture</li>
+                <li data-filter=".shoes">Clothes</li>
                 <li data-filter=".accesories">Accesories</li>
             </ul>
         </div>
