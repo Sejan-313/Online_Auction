2d0db7a5f2c2944dd64ee9a06ca0c3db76bfd57c
Sejan-313 <sezansahin3011@gmail.com>
2025-03-16 15:15:16 +0530

    contact page

--- Frontend/src/role/user/page/contact.jsx
+++ Frontend/src/role/user/page/contact.jsx
@@ -144,24 +144,24 @@ const Contact = () => {
                         <div className="col-lg-3 offset-lg-1">
                             <div className="contact-widget">
                                 <div className="cw-item">
-                                    <h5>Location</h5>
+                                    <h5>Navsari</h5>
                                     <ul>
-                                        <li>1525 Awesome Lane, </li>
-                                        <li>Los Angeles, CA</li>
+                                        <li>3445,LunsiKui</li>
+                                        <li>Navsari,Guj-India</li>
                                     </ul>
                                 </div>
                                 <div className="cw-item">
                                     <h5>Phone</h5>
                                     <ul>
-                                        <li>+1 (603) 535-4592</li>
-                                        <li>+1 (603) 535-4556</li>
+                                        <li>+91 7043834447</li>
+                                        <li>+91 9313033208</li>
                                     </ul>
                                 </div>
                                 <div className="cw-item">
                                     <h5>E-mail</h5>
                                     <ul>
-                                        <li>contact@violetstore.com</li>
-                                        <li>www.violetstore.com</li>
+                                        <li>onlineauction12@gmail.com</li>
+                                        <li>sezan3011@gmail.com</li>
                                     </ul>
                                 </div>
                             </div>
