8604c6c5ab30923b6d293e1b0bd26c8e6e0ee520
Sejan-313 <sezansahin3011@gmail.com>
2025-03-16 14:38:33 +0530

    container removed

--- Frontend/src/role/user/component/Footer.jsx
+++ Frontend/src/role/user/component/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () =>
             </div>
         </div>
         <div className="social-links-warp">
-			<div className="container">
+			<div className="">
 				<div className="social-links">
 					<a href="" className="instagram"><i className="fa fa-instagram"></i><span>instagram</span></a>
 					<a href="" className="pinterest"><i className="fa fa-pinterest"></i><span>pinterest</span></a>
@@ -78,7 +78,7 @@ const Footer = () =>
 				</div>
 			</div>
 
-<div className="container text-center pt-5">
+<div className=" text-center pt-5">
                 <p>
   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
             </p>
